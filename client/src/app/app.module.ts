109d1dd464650281339c02774edaa169f4a2812b
Aleksandr <aperepjolkin@users.noreply.github.com>
2018-04-25 17:19:43 +0300

    Add files via upload

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { AppService } from './app.service';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
